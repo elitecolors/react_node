711dafba10779ccdbaf483ad71d38c812ad1c8b1
elitecolors <saidaniahmed125@gmail.com>
2021-03-20 15:39:40 +0100

    add custom auth middleware with jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,7 +5,7 @@ const bcrypt= require('bcryptjs');
 const  jwt = require('jsonwebtoken');
 const config = require('config');
 const  User= require('../../models/User');
-const {check, validationResult} = require('express-validator/check');
+const {check, validationResult} = require('express-validator');
 // @route   Post api/users
 // @desc    add user
 // @access  Public
