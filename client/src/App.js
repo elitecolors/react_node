e4ec0224d4c46f519927c1cb554add647f342d6a
elitecolors <saidaniahmed125@gmail.com>
2021-03-23 11:00:36 +0100

    create Redux store

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,15 @@ import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 
+// Redux
+import {Provider } from 'react-redux';
+import store from "./store";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 
 const  App = () => (
+    <Provider store={store}>
     <Router>
         <Fragment>
             <Navbar />
@@ -22,6 +26,7 @@ const  App = () => (
                 </section>
         </Fragment>
     </Router>
+    </Provider>
 );
 
 export default App;
