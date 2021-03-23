528062a3f90ea33a623efbe4cc635c01e479948d
elitecolors <saidaniahmed125@gmail.com>
2021-03-23 10:39:11 +0100

    login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { Fragment, useState} from 'react'
+import {Link } from "react-router-dom";
 
 const Login= () =>{
-    return (
-        <div>
-            login
-        </div>
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
 
-    )
+    const {  email,password} = formData;
+    const onChange= e => setFormData({...formData,[e.target.name]: e.target.value});
+    const onSubmit = async e => {
+        e.preventDefault();
+
+            console.log(formData);
+
+    }
+
+    return <Fragment>
+        <h1 className="large text-primary">Sign Up</h1>
+        <p className="lead"><i className="fas fa-user"></i> SIgn Your Account</p>
+        <form className="form" onSubmit={e =>onSubmit(e)}>
+            <div className="form-group">
+                <input type="email"
+                       value={email}
+                       onChange={e => onChange(e)}
+                       placeholder="Email Address"
+                       name="email"/>
+            </div>
+            <div className="form-group">
+                <input
+                    type="password"
+                    value={password}
+                    onChange={e => onChange(e)}
+                    placeholder="Password"
+                    name="password"
+                    minLength="6"
+                />
+            </div>
+            <input type="submit" className="btn btn-primary" value="Login"/>
+        </form>
+        <p className="my-1">
+            Already have an account? <Link to="/register">Sign Up</Link>
+        </p>
+    </Fragment>
 }
 
 export default Login
